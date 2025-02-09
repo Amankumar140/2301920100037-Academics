fcf2aee07e189926c41a185cc8b11c47642d299d
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-02-09 22:54:26 +0530

    Create Reverse a String.java

---
+++ Revision/Reverse a String.java
@@ -0,0 +1,9 @@
+class Solution {
+    static String revStr(String s) {
+         StringBuilder res=new StringBuilder();
+         res.append(s);
+         res.reverse();
+         return res.toString();
+    }
+    
+}
