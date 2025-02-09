0c9890b78e481cb2253d1cd12cd48b889862b70f
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-02-09 23:11:37 +0530

    Update Non Repeating Character.java

--- Revision/Non Repeating Character.java
+++ Revision/Non Repeating Character.java
@@ -1,15 +1,20 @@
+
 class Solution {
-  public:
     // Function to find the first non-repeating character in a string.
-    char nonRepeatingChar(string &s) {
-         int n=s.size();
-         vector<int> v(26,0);
-         for(int i=0; i<n; i++){
-             v[s[i]-'a']++;
-         }
-        for(int i=0; i<n; i++){
-            if(v[s[i]-'a']==1) return s[i];
-        }
+    static char nonRepeatingChar(String s) {
+          int []arr=new int[26];
+          int n=s.length();
+          for(int i=0; i<n; i++){
+              
+              arr[s.charAt(i)-'a']++;
+              
+          }
+          for(int i=0; i<n; i++){
+                if(arr[s.charAt(i)-'a']==1){
+                    return s.charAt(i);
+                }
+          }
           return '$';
+          
     }
-};
+}
